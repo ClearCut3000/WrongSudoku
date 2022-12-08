97c2022be3ffef4590166a4f6862d09dd58192e6
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-08 21:00:54 +0300

    Initial Commit

---
+++ WrongSudoku/WrongSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrongSudokuApp.swift
+//  WrongSudoku
+//
+//  Created by Николай Никитин on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WrongSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
